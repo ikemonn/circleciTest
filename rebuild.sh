1977559eab7982ff085ac9b70dd012ba2b7868a2
ikemonn <daiki.matsui@plaid.co.jp>
2016-01-13 16:04:34 +0900

    refacter

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-MAX_CNT=3
-curr_build_id=$CIRCLE_BUILD_NUM
-build_cnt=0
+MAX_REBUILD_CNT=2 # 最大何回リビルドするか？build + rebuild = 3回で設定
+curr_build_id=$CIRCLE_BUILD_NUM #今回のビルドID
+rebuild_cnt=0 # リビルド回数
 API_END_POINT="https://circleci.com/api/v1/project/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
 
 # 現在のリトライ回数を取得する
@@ -14,16 +14,14 @@ expr "$prev_build_id" + 1 >/dev/null 2>&1
 if [ $? -lt 2 ]; then
   echo "ビルド番号を取得 " $prev_build_id
   echo "前回までのビルド回数を取得します。"
-  ARTIFACTS_NAME="buildCnt"
   # "buildCnt"が含まれているデータを取得し、artifactsのURLを取得
   artifact_url=$(curl -s $API_END_POINT/$prev_build_id/artifacts?circle-token=$CIRCLE_TOKEN|sed -e '1,1d'|jq -r '.[] | select(contains({path:"buildCnt"})) | .url')
   echo URL " $artifact_url"
   # 正しく通信できているか確認(exit codeが0以外だとエラー)
   exit_code=$(curl -f -I $artifact_url)
-  # echo exit_code " $exit_code"
   if [ $? -eq 0 ];then
     echo "curlできているので、ビルド回数を取得"
-    build_cnt=$(curl -s $artifact_url)
+    rebuild_cnt=$(curl -s $artifact_url)
   else
     echo "curl失敗です"
     # TODO: Slackに通知
@@ -32,20 +30,19 @@ else
   echo "ビルド番号を取得できませんでした"
 fi
 
-echo build_cnt $build_cnt
+echo rebuild_cnt $rebuild_cnt
 
 # 取得できれば指定回数以下かチェック、指定回数以下なら+1回をfileに書き込む & retry
 # 数値か判定
-expr "$build_cnt" + 1 >/dev/null 2>&1
+expr "$rebuild_cnt" + 1 >/dev/null 2>&1
 if [ $? -ge 2 ]; then
   echo "buildCntの値が数値ではありません"
   # TODO: Slackに通知
 fi
 
-if [ "$build_cnt" -lt "$MAX_CNT" ]; then
-  build_cnt=$((build_cnt+1))
-  echo $build_cnt > $CIRCLE_ARTIFACTS/buildCnt.txt
-  # TODO: リトライ処理書く
+if [ "$rebuild_cnt" -lt "$MAX_REBUILD_CNT" ]; then
+  rebuild_cnt=$((rebuild_cnt+1))
+  echo $rebuild_cnt > $CIRCLE_ARTIFACTS/buildCnt.txt
   echo "リトライします"
   # キャッシュの削除
   curl -X DELETE $API_END_POINT/build-cache?circle-token=$CIRCLE_TOKEN
@@ -56,5 +53,4 @@ else
   echo "リトライしません"
 fi
 
-echo "終了"
 exit 0
