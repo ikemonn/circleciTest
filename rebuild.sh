66e1301c01897618a46d0e9451c07750a64a1767
ikemonn <daiki.matsui@plaid.co.jp>
2016-01-15 12:19:57 +0900

    ビルド結果をファイルに書き込む

--- rebuild.sh
+++ rebuild.sh
@@ -19,20 +19,25 @@ rebuild_cnt=0 # リビルド回数
 API_END_POINT="https://circleci.com/api/v1/project/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
 CIRCLE_TOKEN_PARAM="circle-token=$CIRCLE_REBUILD_TOKEN"
 BUILD_RESULT_URL="https://circleci.com/gh/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$curr_build_id"
-BUILD_USER_NAME=$(curl -s $API_END_POINT/$curr_build_id?$CIRCLE_TOKEN_PARAM | sed -e '1,1d' | jq -r '.user.login')
+BUILD_RESULT_FILE=$HOME/result.txt
+# ビルド結果は使いまわすのでファイルに書き込む
+curl -s $API_END_POINT/$curr_build_id?$CIRCLE_TOKEN_PARAM | sed -e '1,1d' > $BUILD_RESULT_FILE
+head $BUILD_RESULT_FILE
+BUILD_USER_NAME=$(cat $BUILD_RESULT_FILE | sed -e '1,1d' | jq -r '.user.login')
+BUILD_BRANCH=$(cat $BUILD_RESULT_FILE | jq -r '.branch')
 
 # 今回のビルドが成功しているかを確認
 # APIから取得できる配列内の、status:failedの数を確認する
-test_fail_cnt=$(curl -s $API_END_POINT/$curr_build_id?$CIRCLE_TOKEN_PARAM | sed -e '1,1d' | jq '[.steps[].actions[] | select(contains({status:"failed"})) | .status] | length')
+test_fail_cnt=$(cat $BUILD_RESULT_FILE | sed -e '1,1d' | jq '[.steps[].actions[] | select(contains({status:"failed"})) | .status] | length')
 
 # pullreqのnumberを取得する
 # https://github.com/ikemonn/circleciTest/pull/1 の形で来るので、末尾だけ取得
-pull_request_num=$(curl -s $API_END_POINT/$curr_build_id?$CIRCLE_TOKEN_PARAM | sed -e '1,1d' | jq -r '.pull_request_urls[]' | awk -F / '{print $NF}')
+pull_request_num=$(cat $BUILD_RESULT_FILE | sed -e '1,1d' | jq -r '.pull_request_urls[]' | awk -F / '{print $NF}')
 echo pull_request_num $pull_request_num
 echo test_fail_cnt $test_fail_cnt
 if [ $test_fail_cnt -le 0 ]; then
   echo "Testはすべて成功です！"
-  notify_to_slack ":white_check_mark: CircleCIのテストが成功しました！ :white_check_mark:" $BUILD_RESULT_URL $BUILD_USER_NAME "good"
+  notify_to_slack ":white_check_mark: CircleCIのテストが成功しました！($BUILD_BRANCH) :white_check_mark:" $BUILD_RESULT_URL $BUILD_USER_NAME "good"
   comment_pull_request $pull_request_num "true" $curr_build_id $BUILD_RESULT_URL
   exit 0
 fi
@@ -41,7 +46,7 @@ echo $test_fail_cnt"個失敗しているテストがあります"
 
 # 現在のリトライ回数を取得する
 # 前回のビルド番号
-prev_build_id=$(curl -s $API_END_POINT/$curr_build_id?$CIRCLE_TOKEN_PARAM | sed -e '1,1d' | jq '.retry_of')
+prev_build_id=$(cat $BUILD_RESULT_FILE | sed -e '1,1d' | jq '.retry_of')
 echo prev_build_id $prev_build_id
 # nullかビルド番号が返ってくるので、数値か文字列かを判定
 expr "$prev_build_id" + 1 >/dev/null 2>&1
@@ -89,6 +94,6 @@ if [ "$rebuild_cnt" -lt "$MAX_REBUILD_CNT" ]; then
   curl -X POST $API_END_POINT/$curr_build_id/retry?$CIRCLE_TOKEN_PARAM
 else
   echo "指定回数以上リトライ済みなので、リトライしません"
-  notify_to_slack ":fire: CircleCIのテストが失敗しました :fire:" $BUILD_RESULT_URL $BUILD_USER_NAME
+  notify_to_slack ":fire: CircleCIのテストが失敗しました($BUILD_BRANCH) :fire:" $BUILD_RESULT_URL $BUILD_USER_NAME
   comment_pull_request $pull_request_num "false" $curr_build_id $BUILD_RESULT_URL
 fi
