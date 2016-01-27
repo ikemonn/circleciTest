d723c314f969fbd77c6effc202f57fcc26c4ab22
ikemonn <daiki.matsui@plaid.co.jp>
2016-01-27 11:21:16 +0900

    add

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-mocha --compilers coffee:coffee-script/register test.coffee
-echo 0
+# mocha --compilers coffee:coffee-script/register test.coffee
+node_modules/mocha/bin/mocha --compilers coffee:coffee-script/register test.coffee
+# echo $?
+if [ $? -eq 1 ]; then
+  echo "とおた"
+  node_modules/mocha/bin/mocha --compilers coffee:coffee-script/register test.coffee
+  exit 0
+else
+  echo "とおてない"
+fi
