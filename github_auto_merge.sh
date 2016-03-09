1ccb9b4f37bf62837ff444ec55138da0c5a5f1ad
ikemonn <circleci@plaid.co.jp>
2016-03-09 19:54:25 +0900

    daf

--- github_auto_merge.sh
+++ github_auto_merge.sh
@@ -8,11 +8,12 @@ check_label() {
   local TARGET_LABEL=$1
   local PULL_REQUEST_NUM=$2
   local END_POINT="https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
-  local LABEL_LIST=$(curl -s -H "Authorization:token $GITHUB_API_TOKEN" $END_POINT/issues/$PULL_REQUEST_NUM | jq -r '.user.login')
-  echo $LABEL_LIST
+  local LABEL_LIST=$(curl $END_POINT/issues/$PULL_REQUEST_NUM | jq -r '.user.login')
+  # local LABEL_LIST=$(curl -s -H "Authorization:token $GITHUB_API_TOKEN" $END_POINT/issues/$PULL_REQUEST_NUM | jq -r '.user.login')
+  echo "リスト:" $LABEL_LIST
   for label in $LABEL_LIST;
   do
-    echo $label
+    echo "らべる: " $label
     if [ "$label" = "$TARGET_LABEL" ]; then
       echo "自動マージ用に設定されたラベルがありました。マージします。"
       exit 0
