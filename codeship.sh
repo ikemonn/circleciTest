99504e73eedc13090353124f2a29b8d9fc70613b
ikemonn <circleci@plaid.co.jp>
2016-03-18 18:00:30 +0900

    daf

--- codeship.sh
+++ codeship.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 echo "This is codeship test"
+printenv
+which jq
+curl https://codeship.com/api/v1/projects/${PROJECT_NUM}.json?api_key=${API_KEY} | jq -r ".builds[0] | select({'id': $CI_BUILD_NUMBER}) | .status"
