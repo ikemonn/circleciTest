b05c29a912b5a2c8ec1265527935befbc5df3b8f
ikemonn <circleci@plaid.co.jp>
2016-06-29 11:07:41 +0900

    add chromedriver

---
+++ install_chromedriver.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+version=$(curl -s http://chromedriver.storage.googleapis.com/LATEST_REASE)
+
+if [[ "$version" =~ ^[0-9]+\.[0-9].*$ ]]; then
+   echo "chromedriver version is " $version
+else
+   echo "Invalid chromedriver version!! Get latest version may be failed. Fix it." $version
+   exit 1
+fi
+wget https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
